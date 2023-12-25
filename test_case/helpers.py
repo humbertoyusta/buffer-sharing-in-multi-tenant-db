8ed0ac24d6e4622059d89419f026817486ee0d4f
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-25 23:36:37 +0100

    fix/normal-giving-max-because-of--1

--- test_case/helpers.py
+++ test_case/helpers.py
@@ -9,7 +9,7 @@ def generate_uniform_array (length: int, max_value: int) -> np.array:
     return np.random.randint(1, max_value + 1, length)
 
 def generate_normal_array (variance: float, length: int, max_value: int) -> np.array:
-    return (abs(np.random.normal(0, variance, length)).astype(int) - 1) % max_value + 1
+    return (abs(np.random.normal(0, variance, length)).astype(int)) % max_value + 1
 
 def generate_pareto_array (parameter: float, length: int, max_value: int) -> np.array:
     return (np.random.pareto(parameter, length) % max_value).astype(int) + 1
