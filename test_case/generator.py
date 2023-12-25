3afb3de0c66f54f650919b7460cb19acb660990f
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-25 16:20:44 +0100

    feat/add-test-case-generator

---
+++ test_case/generator.py
@@ -0,0 +1,135 @@
+from test_case.helpers import *
+import numpy as np
+from importlib import import_module
+from test_case import constants
+
+class Generator:
+    def __init__(self, test_number: int):
+        self.test_number = test_number
+        self.test_case = import_module(f"test_cases.test_case_{test_number}.description")
+        if self.test_case is None:
+            raise ModuleNotFoundError(f"Test case {test_number} description does not exist.")
+        self.total_buffer_size = self.test_case.total_buffer_size
+        self.tenants = self.test_case.tenants
+        self.input_file = f"test_cases/test_case_{test_number}/input.txt"
+        np.random.seed(0)
+
+    def generate_test_case(self):
+        for tenant in self.test_case.tenants:
+            current_tenant_page_accesses = []
+            current_tenant_page_accesses_sum = 0
+
+            while current_tenant_page_accesses_sum < tenant['page_accesses_dist']['length']:
+
+                batch_pages = np.array([])
+                if tenant['page_accesses_dist']['type'] == 'zipfian':
+                    batch_pages = generate_zipfian_array(
+                        tenant['page_accesses_dist']['parameter'], 
+                        constants.BATCH_LENGTH, 
+                        tenant['database_size'],
+                    )
+                elif tenant['page_accesses_dist']['type'] == 'uniform':
+                    batch_pages = generate_uniform_array(
+                        constants.BATCH_LENGTH, 
+                        tenant['database_size'],
+                    )
+                elif tenant['page_accesses_dist']['type'] == 'normal':
+                    batch_pages = generate_normal_array(
+                        tenant['page_accesses_dist']['variance'], 
+                        constants.BATCH_LENGTH, 
+                        tenant['database_size'],
+                    )
+                elif tenant['page_accesses_dist']['type'] == 'pareto':
+                    batch_pages = generate_pareto_array(
+                        tenant['page_accesses_dist']['parameter'], 
+                        constants.BATCH_LENGTH, 
+                        tenant['database_size'],
+                    )
+                else:
+                    raise Exception(f"Unknown distribution type: {tenant['page_accesses_dist']['type']}")
+                
+                batch_interval_lengths = np.array([])
+                if tenant['page_accesses_interval_dist']['type'] == 'uniform':
+                    batch_interval_lengths = generate_uniform_array(
+                        constants.BATCH_LENGTH, 
+                        tenant['page_accesses_interval_dist']['max_value'],
+                    )
+                elif tenant['page_accesses_interval_dist']['type'] == 'normal':
+                    batch_interval_lengths = generate_normal_array(
+                        tenant['page_accesses_interval_dist']['variance'], 
+                        constants.BATCH_LENGTH, 
+                        tenant['page_accesses_interval_dist']['max_value'],
+                    )
+                elif tenant['page_accesses_interval_dist']['type'] == 'pareto':
+                    batch_interval_lengths = generate_pareto_array(
+                        tenant['page_accesses_interval_dist']['parameter'], 
+                        constants.BATCH_LENGTH, 
+                        tenant['page_accesses_interval_dist']['max_value'],
+                    )
+                elif tenant['page_accesses_interval_dist']['type'] == 'zipfian':
+                    batch_interval_lengths = generate_zipfian_array(
+                        tenant['page_accesses_interval_dist']['parameter'], 
+                        constants.BATCH_LENGTH, 
+                        tenant['page_accesses_interval_dist']['max_value'],
+                    )
+                else:
+                    raise Exception(f"Unknown distribution type: {tenant['page_accesses_interval_dist']['type']}")
+
+                batch = np.column_stack((batch_pages, batch_interval_lengths))
+                
+                for batch_row in batch:
+                    if current_tenant_page_accesses_sum + batch_row[1] <= tenant['page_accesses_dist']['length']:
+                        current_tenant_page_accesses.append([batch_row[0], batch_row[1]])
+                        current_tenant_page_accesses_sum += batch_row[1]
+                    else:
+                        current_tenant_page_accesses.append([batch_row[0], tenant['page_accesses_dist']['length'] - current_tenant_page_accesses_sum])
+                        current_tenant_page_accesses_sum = tenant['page_accesses_dist']['length']
+                        break
+
+            current_tenant_page_accesses = np.array(current_tenant_page_accesses)
+
+            if tenant['pages_should_be_renumbered']:
+                permutation = np.random.permutation(tenant['database_size'] + 1)
+                current_tenant_page_accesses[:, 0] = permutation[current_tenant_page_accesses[:, 0].astype(int)]
+
+            current_tenant_page_accesses = np.column_stack((
+                np.full(current_tenant_page_accesses.shape[0], tenant['tenant_id']), 
+                current_tenant_page_accesses[:, 0], 
+                current_tenant_page_accesses[:, 1],
+            ))
+
+            for page_access in current_tenant_page_accesses:
+                assert page_access[0] == tenant['tenant_id'], "Tenant ID mismatch."
+                assert page_access[1] >= 1 and page_access[1] <= tenant['database_size'], "Page ID out of range."
+            assert sum(current_tenant_page_accesses[:, 2]) == tenant['page_accesses_dist']['length'], "Page access length mismatch."
+
+            tenant['page_accesses'] = current_tenant_page_accesses
+                
+        compressed_page_accesses = np.concatenate([tenant['page_accesses'] for tenant in self.test_case.tenants])
+        np.random.shuffle(compressed_page_accesses)
+        
+        full_page_accesses = []
+
+        for page_access in compressed_page_accesses:
+            current_page = page_access[1]
+            for _ in range(page_access[2]):
+                full_page_accesses.append([page_access[0], current_page])
+                current_page += 1
+                if current_page > self.test_case.tenants[page_access[0] - 1]['database_size']:
+                    current_page = 1
+
+        count_per_tenant = np.zeros(len(self.test_case.tenants) + 1)
+        for page_access in full_page_accesses:
+            assert page_access[0] >= 1 and page_access[0] <= len(self.test_case.tenants), "Tenant ID out of range."
+            assert page_access[1] >= 1 and page_access[1] <= self.test_case.tenants[page_access[0] - 1]['database_size'], "Page ID out of range."
+            count_per_tenant[page_access[0]] += 1
+        for tenant in range(1, len(self.test_case.tenants) + 1):
+            assert count_per_tenant[tenant] == self.test_case.tenants[tenant - 1]['page_accesses_dist']['length'], "Page access length mismatch."
+        
+
+        return {
+            'total_buffer_size': self.test_case.total_buffer_size,
+            'tenants': self.test_case.tenants,
+            'page_accesses': np.array(full_page_accesses),
+        }
+            
