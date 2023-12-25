3f6ba3c5b412d574ba1949d6d890fff3acf32e60
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-25 16:21:48 +0100

    feat/add-helpers-for-random-generation

---
+++ test_case/helpers.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+np.random.seed(0)
+
+def generate_zipfian_array (parameter: float, length: int, max_value: int) -> np.array:
+    return (np.random.zipf(parameter, length) - 1) % max_value + 1
+
+def generate_uniform_array (length: int, max_value: int) -> np.array:
+    return np.random.randint(1, max_value + 1, length)
+
+def generate_normal_array (variance: float, length: int, max_value: int) -> np.array:
+    return (abs(np.random.normal(0, variance, length)).astype(int) - 1) % max_value + 1
+
+def generate_pareto_array (parameter: float, length: int, max_value: int) -> np.array:
+    return (np.random.pareto(parameter, length) % max_value).astype(int) + 1
