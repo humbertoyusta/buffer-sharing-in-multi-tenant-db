f45ddf74d15ef53472498cbf0646d2104e05920e
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-26 12:07:46 +0100

    feat/add-test-case-3

--- gen_script.py
+++ gen_script.py
@@ -4,26 +4,8 @@ from test_case.io_wrapper import IoWrapper
 from test_case.page_accesses_plotter import PageAccessesPlotter
 
 if __name__ == "__main__":
-    for test_number in range(1, 3):
+    for test_number in range(1, 4):
         test_case = Generator(test_number).generate_test_case()
         IoWrapper(test_case, test_number).print_test_case()
         Validator(test_number).validate()
         PageAccessesPlotter(test_case['page_accesses'], len(test_case['tenants']), test_number).plot()
-    # page_accesses = [
-    #     (1, 1),
-    #     (2, 1),
-    #     (1, 3),
-    #     (1, 1),
-    #     (2, 1),
-    #     (2, 2),
-    #     (2, 3),
-    #     (2, 1),
-    #     (2, 1),
-    #     (3, 2),
-    #     (3, 3),
-    #     (3, 1),
-    #     (3, 1),
-    #     (2, 2),
-    #     (2, 3),
-    # ]
-    # PageAccessesPlotter(page_accesses, 3, 1).plot()
---
+++ test_cases/test_case_3/description.py
@@ -0,0 +1,66 @@
+## Description of the test case 3
+## Three tenants following distributions similar to 
+## Taras Basiuk, Le Gruenwald, Laurent d’Orazio, Eleazar Leal. A Persistent Memory-Aware Buffer
+## Pool Manager Simulator for Multi-Tenant Cloud Databases. International Workshop on Big Data
+## Management on Emerging Hardware, Apr 2020, Dallas, United States. ffhal-02514273f 
+
+tenants = [
+    {
+        'tenant_id': 1,
+        'priority_level': 1,
+        'database_size': 10000,
+        'minimum_buffer_size': 100,
+        'base_buffer_size': 1000,
+        'maximum_buffer_size': 3000,
+        'page_accesses_dist': {
+            'type': 'uniform',
+            'length': 40000,
+        },
+        'pages_should_be_renumbered': False,
+        'page_accesses_interval_dist': {
+            'type': 'uniform',
+            'length': 40000,
+            'max_value': 1,
+        },
+    },
+    {
+        'tenant_id': 2,
+        'priority_level': 4,
+        'database_size': 20000,
+        'minimum_buffer_size': 1000,
+        'base_buffer_size': 5000,
+        'maximum_buffer_size': 18000,
+        'page_accesses_dist': {
+            'type': 'normal',
+            'variance': 2000,
+            'length': 200000,
+        },
+        'pages_should_be_renumbered': False,
+        'page_accesses_interval_dist': {
+            'type': 'uniform',
+            'length': 200000,
+            'max_value': 1,
+        },
+    },
+    {
+        'tenant_id': 3,
+        'priority_level': 10,
+        'database_size': 100000,
+        'minimum_buffer_size': 10000,
+        'base_buffer_size': 20000,
+        'maximum_buffer_size': 60000,
+        'page_accesses_dist': {
+            'type': 'pareto',
+            'parameter': 0.02,
+            'length': 760000,
+        },
+        'pages_should_be_renumbered': False,
+        'page_accesses_interval_dist': {
+            'type': 'uniform',
+            'length': 760000,
+            'max_value': 1,
+        },
+    },
+]
+
+total_buffer_size = int(sum([tenant['base_buffer_size'] for tenant in tenants]) * 0.8)
