3e655943d6ef598e2c2e68be86417bf3571057fe
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-29 23:35:01 +0100

    fix/run-script

--- run.sh
+++ run.sh
@@ -2,11 +2,15 @@
 
 sol="lru_policy_1_solution"
 
-first_test=$1
-last_test=$2
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    first_test=1
+else
+    first_test=$1
+fi
+if ! [[ $2 =~ ^[0-9]+$ ]]; then
+    last_test=14
+else
+    last_test=$2
+fi
 
-for i in $(seq "$first_test" "$last_test")
-do
-    echo "Running test $i"
-    ./build/src/main "$sol" "$i"
-done
+./build/src/main "$sol" "$first_test" "$last_test"
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,27 @@
 
 int main(int argc, char **argv) {
   std::string solution_name = argv[1];
-  int test_number = std::stoi(argv[2]);
+  int first_test_number = std::stoi(argv[2]);
+  int last_test_number = std::stoi(argv[3]);
 
-  auto input_reader = InputReader();
-  auto [tenants, page_accesses, total_buffer_size] =
-      input_reader.ReadInput(test_number);
+  for (int test_number = first_test_number; test_number <= last_test_number;
+       test_number++) {
+    auto input_reader = InputReader();
+    auto [tenants, page_accesses, total_buffer_size] =
+        input_reader.ReadInput(test_number);
 
-  auto checker = Checker();
-  Solution *solution = nullptr;
+    auto checker = Checker();
+    Solution *solution = nullptr;
 
-  if (solution_name == "lru_policy_1_solution") {
-    solution = new LruPolicy1Solution();
-  } else {
-    std::cout << "Unknown solution name: " << solution_name << std::endl;
-    return 1;
+    if (solution_name == "lru_policy_1_solution") {
+      solution = new LruPolicy1Solution();
+    } else {
+      std::cout << "Unknown solution name: " << solution_name << std::endl;
+      return 1;
+    }
+
+    checker.CheckSolution(solution, tenants, page_accesses, total_buffer_size);
+
+    delete solution;
   }
-
-  checker.CheckSolution(solution, tenants, page_accesses, total_buffer_size);
 }
