735643f938e76591c8dbd8f1e6c9b3647d41f5ce
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-29 19:06:36 +0100

    feat/add-==-operator-to-page-access

--- src/types/page_access.h
+++ src/types/page_access.h
@@ -8,6 +8,10 @@ struct PageAccess {
   PageAccess(int t_tenant_id, int t_page_id);
 
   PageAccess();
+
+  bool operator==(const PageAccess &other) const {
+    return tenant_id == other.tenant_id && page_id == other.page_id;
+  }
 };
 
 #endif // PAGE_ACCESS_H
