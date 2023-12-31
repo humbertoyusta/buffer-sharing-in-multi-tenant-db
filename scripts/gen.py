b65009e4ce9f927c593d1f48603f66754c002f13
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-31 23:12:47 +0100

    feat/take-test-generation-from-console-parameter

--- scripts/gen.py
+++ scripts/gen.py
@@ -2,9 +2,13 @@ from .test_case.validator import Validator
 from .test_case.generator import Generator
 from .test_case.io_wrapper import IoWrapper
 from .test_case.page_accesses_plotter import PageAccessesPlotter
+import sys
 
 if __name__ == "__main__":
-    for test_number in range(1, 15):
+    first_test = sys.argv[1] if len(sys.argv) > 1 else 1
+    last_test = sys.argv[2] if len(sys.argv) > 2 else 14
+
+    for test_number in range(int(first_test), int(last_test) + 1):
         test_case = Generator(test_number).generate_test_case()
         IoWrapper(test_case, test_number).print_test_case()
         Validator(test_number).validate()
