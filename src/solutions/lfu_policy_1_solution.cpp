7c1a4a5961ff5632484a8e13275790d8ae737f14
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-01-01 19:29:50 +0100

    feat/optimize-lfu-1

--- src/solutions/lfu_policy_1_solution.cpp
+++ src/solutions/lfu_policy_1_solution.cpp
@@ -13,12 +13,14 @@ void LfuPolicy1Solution::Init(const std::vector<Tenant> &tenants,
   faults_in_judge_.clear();
   faults_in_solution_.clear();
   cache_used_per_tenant_.clear();
+  tenant_scores_.clear();
 
   for (const auto &tenant : tenants) {
     judge_caches_.emplace_back(tenant.base_buffer_size);
     faults_in_judge_.push_back(0);
     faults_in_solution_.push_back(0);
     cache_used_per_tenant_.push_back(0);
+    tenant_scores_.push_back(0);
   }
 }
 
@@ -38,6 +40,8 @@ std::pair<int, bool> LfuPolicy1Solution::AccessPage(PageAccess page_access) {
 
   if (found.second) {
     lfu_cache_.IncreasePageFrequency(page_access);
+
+    UpdateScore(page_access.tenant_id);
     return {found.first, true};
   } else {
 
@@ -48,43 +52,28 @@ std::pair<int, bool> LfuPolicy1Solution::AccessPage(PageAccess page_access) {
       lfu_cache_.AddPage(page_access, available_location);
       ++cache_used_per_tenant_[page_access.tenant_id - 1];
       ++faults_in_solution_[page_access.tenant_id - 1];
+
+      UpdateScore(page_access.tenant_id);
       return {available_location, false};
     }
 
-    std::vector<double> tenant_scores;
-    for (const auto &tenant : tenants_) {
-      if (faults_in_judge_[tenant.tenant_id - 1] == 0 ||
-          tenant.base_buffer_size == 0) {
-        tenant_scores.push_back(INT_MAX);
+    int tenant_id_to_evict = -1;
+    for (int tenant = 0; tenant < tenants_.size(); ++tenant) {
+      if (cache_used_per_tenant_[tenant] < tenants_[tenant].min_buffer_size ||
+          (cache_used_per_tenant_[tenant] == tenants_[tenant].min_buffer_size &&
+           tenant + 1 != page_access.tenant_id)) {
         continue;
-      } else {
-        tenant_scores.push_back(tenant_selection_policy_.TenantScore(
-            faults_in_solution_[tenant.tenant_id - 1],
-            faults_in_judge_[tenant.tenant_id - 1],
-            cache_used_per_tenant_[tenant.tenant_id - 1],
-            tenant.base_buffer_size, tenant.priority));
-      }
-    }
-
-    for (int i = 0; i < tenants_.size(); ++i) {
-      if (cache_used_per_tenant_[i] < tenants_[i].min_buffer_size ||
-          (cache_used_per_tenant_[i] == tenants_[i].min_buffer_size &&
-           i + 1 != page_access.tenant_id)) {
-        tenant_scores[i] = INT_MAX;
       }
       if (cache_used_per_tenant_[page_access.tenant_id - 1] >=
               tenants_[page_access.tenant_id - 1].max_buffer_size &&
-          i + 1 != page_access.tenant_id) {
-        tenant_scores[i] = INT_MAX;
+          tenant + 1 != page_access.tenant_id) {
+        continue;
+      }
+      if (tenant_id_to_evict == -1 ||
+          tenant_scores_[tenant] < tenant_scores_[tenant_id_to_evict - 1]) {
+        tenant_id_to_evict = tenant + 1;
       }
     }
-
-    assert(*std::min_element(tenant_scores.begin(), tenant_scores.end()) !=
-           INT_MAX);
-
-    int tenant_id_to_evict =
-        std::min_element(tenant_scores.begin(), tenant_scores.end()) -
-        tenant_scores.begin() + 1;
 
     Lfu::Page evicted_page =
         lfu_cache_.EvictLeastFrequentlyUsedPage(tenant_id_to_evict);
@@ -100,6 +89,19 @@ std::pair<int, bool> LfuPolicy1Solution::AccessPage(PageAccess page_access) {
     assert(cache_used_per_tenant_[tenant_id_to_evict - 1] >=
            tenants_[tenant_id_to_evict - 1].min_buffer_size);
 
+    UpdateScore(page_access.tenant_id);
     return {evicted_page.buffer_location, false};
   }
+}
+
+void LfuPolicy1Solution::UpdateScore(int tenant_id) {
+  if (faults_in_judge_[tenant_id - 1] == 0) {
+    tenant_scores_[tenant_id - 1] = 0;
+    return;
+  }
+  tenant_scores_[tenant_id - 1] = tenant_selection_policy_.TenantScore(
+      faults_in_solution_[tenant_id - 1], faults_in_judge_[tenant_id - 1],
+      cache_used_per_tenant_[tenant_id - 1],
+      tenants_[tenant_id - 1].base_buffer_size,
+      tenants_[tenant_id - 1].priority);
 }
