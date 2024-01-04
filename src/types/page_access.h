1c3baefdf0de4300290a945552ad6e92f304af09
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-01-04 20:18:33 +0100

    feat/adding-hash-to-page-access

--- src/types/page_access.h
+++ src/types/page_access.h
@@ -1,6 +1,8 @@
 #ifndef PAGE_ACCESS_H
 #define PAGE_ACCESS_H
 
+#include <functional>
+
 struct PageAccess {
   int tenant_id;
   int page_id;
@@ -13,5 +15,16 @@ struct PageAccess {
     return tenant_id == other.tenant_id && page_id == other.page_id;
   }
 };
+
+namespace std {
+template <> struct hash<PageAccess> {
+  size_t operator()(const PageAccess &page_access) const {
+    size_t h1 = std::hash<int>()(page_access.tenant_id);
+    size_t h2 = std::hash<int>()(page_access.page_id);
+
+    return h1 ^ (h2 << 1);
+  }
+};
+} // namespace std
 
 #endif // PAGE_ACCESS_H
