7688367e2077db42480c4e7769044a7830b6befc
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-02-26 19:49:40 +0100

    fix/make-folder-if-does-not-exist-in-io-wrapper

--- scripts/test_case/io_wrapper.py
+++ scripts/test_case/io_wrapper.py
@@ -1,3 +1,6 @@
+import os
+
+
 class IoWrapper:
     def __init__(self, test_case, test_number: int, test_type: str):
         self.test_case = test_case
@@ -5,6 +8,8 @@ class IoWrapper:
         self.input_file = f"test_cases/{test_type}/test_case_{test_number}/input.txt"
 
     def print_test_case(self):
+        os.makedirs(os.path.dirname(self.input_file), exist_ok=True)
+
         with open(self.input_file, "w") as f:
             f.write(
                 f"{len(self.test_case['tenants'])} {self.test_case['total_buffer_size']} {len(self.test_case['page_accesses'])}\n"
