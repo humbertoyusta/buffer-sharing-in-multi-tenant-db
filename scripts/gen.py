b0b9b39f3c6d4803a081ae357e70736371e51e2c
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-02-28 20:00:36 +0100

    feat/generate-citibike-cases

--- scripts/gen.py
+++ scripts/gen.py
@@ -4,12 +4,15 @@ from .test_case.io_wrapper import IoWrapper
 from .test_case.page_accesses_plotter import PageAccessesPlotter
 import sys
 from .test_case.brightkite_generator import BrightkiteGenerator
+from .test_case.citibike_generator import CitibikeGenerator
+
+TEST_CASES_PER_CITIBIKE_EXP = 4
 
 if __name__ == "__main__":
     test_type_input = sys.argv[1] if len(sys.argv) > 1 else "all"
 
     if test_type_input == "all":
-        test_types = ["random", "brightkite", "citbike"]
+        test_types = ["random", "brightkite", "citibike"]
     else:
         test_types = [test_type_input]
 
@@ -46,3 +49,32 @@ if __name__ == "__main__":
                 test_number,
                 "brightkite",
             ).plot()
+
+    if "citibike" in test_types:
+        first_exp = sys.argv[2] if len(sys.argv) > 2 else 1
+        last_exp = sys.argv[3] if len(sys.argv) > 3 else 3
+
+        for exp_number in range(int(first_exp), int(last_exp) + 1):
+            for test_number in range(1, TEST_CASES_PER_CITIBIKE_EXP + 1):
+                test_case = CitibikeGenerator(
+                    exp_number=exp_number, test_number=test_number
+                ).generate_test_case()
+                IoWrapper(
+                    test_case=test_case,
+                    test_number=test_number,
+                    test_type="citibike",
+                    exp_number=exp_number,
+                ).print_test_case()
+                Validator(
+                    test_number=test_number,
+                    test_type="citibike",
+                    has_description=False,
+                    exp_number=exp_number,
+                ).validate()
+                PageAccessesPlotter(
+                    page_accesses=test_case["page_accesses"],
+                    tenant_number=len(test_case["tenants"]),
+                    test_number=test_number,
+                    test_type="citibike",
+                    exp_number=exp_number,
+                ).plot()
